652c20adbcba88a41cb304ef604e25eadf332d7b
Haroon Ahmed <haroonrmit@gmail.com>
2018-09-20 00:23:54 +0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log('Starting App')
+
+const _ = require('lodash');
+const fs = require('fs');
+
+const notes = require('./notes');
---
+++ notes.js
@@ -0,0 +1,6 @@
+console.log('starting notes.js');
+
+module.exports.addNotes = () => {
+  console.log('add note');
+  return 'New Note';
+}
